298d6fbbc11068c2386dcfb316d6bb8a2bf7765e
Manel <manel@henkoaching.dev>
2026-05-29 10:43:37 +0200

    Nav: intercambiar orden Portal de empleo y Contenido

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -42,10 +42,10 @@ export default async function MainLayout({ children }: { children: React.ReactNo
       ],
     },
     {
-      title: 'Contenido',
+      title: 'Portal de empleo',
       items: [
-        { href: '/dashboard/blog', label: 'Blog', icon: <Icon path="M4 6h16M4 12h10M4 18h7M17 14l4 4-4 4M21 18h-7" /> },
-        { href: '/dashboard/testimonios', label: 'Testimonios', icon: <Icon path="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" /> },
+        { href: '/dashboard/ofertas', label: 'Ofertas de empleo', icon: <Icon path="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /> },
+        { href: '/dashboard/solicitudes', label: 'Solicitudes', icon: <Icon path="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" /> },
       ],
     },
     {
@@ -55,10 +55,10 @@ export default async function MainLayout({ children }: { children: React.ReactNo
       ],
     },
     {
-      title: 'Portal de empleo',
+      title: 'Contenido',
       items: [
-        { href: '/dashboard/ofertas', label: 'Ofertas de empleo', icon: <Icon path="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /> },
-        { href: '/dashboard/solicitudes', label: 'Solicitudes', icon: <Icon path="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" /> },
+        { href: '/dashboard/blog', label: 'Blog', icon: <Icon path="M4 6h16M4 12h10M4 18h7M17 14l4 4-4 4M21 18h-7" /> },
+        { href: '/dashboard/testimonios', label: 'Testimonios', icon: <Icon path="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" /> },
       ],
     },
   ]
